afe3b5ff14a6a20b7370e86c86d9dfbabc95a265
sarveshatale-stack <71748819+sarveshatale-stack@users.noreply.github.com>
2020-09-28 21:21:38 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+// import "./components/Table.css";
+import ListOfEmployee from "./components/ListOfEmployee";
+export default function App() {
+  return (
+    <div className="App">
+      <ListOfEmployee />
+    </div>
+  );
+}
---
+++ src/components/ListOfEmployee.js
@@ -0,0 +1,36 @@
+import React from "react";
+// import "./Table.css";
+const mystyle = {
+  border: "1px solid gray",
+  width: "500px"
+};
+
+function ListOfEmployee() {
+  return (
+    <div>
+      <table id="listtable" style={mystyle}>
+        <tr className="listtable">
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+        <tr className="listtable">
+          <td>Charlie</td>
+          <td>Janitor</td>
+        </tr>
+        <tr>
+          <td>Mac</td>
+          <td>Bouncer</td>
+        </tr>
+        <tr>
+          <td>Dee</td>
+          <td>Aspiring actress</td>
+        </tr>
+        <tr>
+          <td>Dennis</td>
+          <td>Bartender</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+export default ListOfEmployee;
